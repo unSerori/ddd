003330b9601f9f4dfe07acf3c3e17bec13220244
unSerori <pd.ai.374005475@gmail.com>
2024-09-27 20:35:04 +0900

    [update]初期化処理失敗時はpanicで終了

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main // package
 
 import (
-	"ddd/common/logging" // main method
+	"ddd/common/logging"
 
 	"github.com/gin-gonic/gin"
 )
 
+// main method
 func main() {
 	// 初期化処理
 	initInstances, err := Init() // add "initInstances, " when changing to ddd
 	if err != nil {
-		return // HACK: panic
+		logging.ErrorLog("Failed to initialize.", err)
+		panic(err)
 	}
 	// 破棄処理
 	defer logging.LogFile().Close() // defer文でこの関数終了時に破棄
